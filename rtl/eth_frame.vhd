41305e6a4d096eeebc2df8f684e972ba4e53a9d4
Rahul Ramachandran <rahulram07r@gmail.com>
2025-09-18 18:07:45 +1000

    crc32 works - tested against online crc32/iso-hdlc

--- rtl/eth_frame.vhd
+++ rtl/eth_frame.vhd
@@ -26,10 +26,31 @@ architecture behavioural of eth_frame is
     signal length_count : integer range 0 to 2 := 0;
     signal data_count : integer range 0 to 20 := 0;
     -- CRC signals
-    signal crc_poly : std_logic_vector(31 downto 0) := x"EDB88320";
-    signal crc_final : std_logic_vector(31 downto 0) := x"00000000";
+    signal sig_crc : std_logic_vector(31 downto 0) := (others => '1');
+    signal sig_running_crc : std_logic_vector(31 downto 0) := (others => '1'); -- to probe the var
+    -- function to calculate CRC
+    function crc32_update(
+        crc     : std_logic_vector(31 downto 0);
+        data    : std_logic_vector(7 downto 0)
+    ) return std_logic_vector is
+        constant POLY   : std_logic_vector(31 downto 0) := x"EDB88320";
+        variable c      : std_logic_vector(31 downto 0) := crc;
+        variable b      : std_logic_vector(7 downto 0) := data;
+        variable fb     : std_logic; -- feedback bit
+    begin
+        for i in 0 to 7 loop
+            fb := c(0) xor b(i);        -- feedback bit is the xor of lsb of crc and data bit
+            c := '0' & c(31 downto 1);  -- shift right
+            if fb = '1' then
+                c := c xor POLY;
+            end if;
+        end loop;
+        return c;
+    end function;
 begin
     frame_gen: process(tx_clk)
+        variable var_data : std_logic_vector(7 downto 0) := x"00";
+        variable var_crc : std_logic_vector(31 downto 0) := (others => '1');
     begin
         if rising_edge(tx_clk) then
             case(state) is
@@ -38,7 +59,7 @@ begin
                     -- preamble is always alternating 0s and 1s
                     if (i_en = '1') then
                         state       <= PREAMBLE;
-                        sig_data    <= "10101010";
+                        var_data    := "10101010";
                     end if;
                 
                 when PREAMBLE =>
@@ -47,27 +68,27 @@ begin
                     -- logic at the very end and the second in the IDLE stage)
                     -- so only 6 bytes left to send
                     if preamble_count < 5 then
-                        sig_data <= "10101010";
+                        var_data := "10101010";
                         preamble_count <= preamble_count + 1;
                     else
                         -- this would be the last preamble byte left
-                        sig_data <= "10101010";
+                        var_data := "10101010";
                         state <= SFD;
                     end if;
                 
                 when SFD =>
                     -- SFD is always 10101011
-                    sig_data <= "10101011";
+                    var_data := "10101011";
                     state <= DEST_ADDR;
                 
                 when DEST_ADDR =>
                     -- hardcoded MAC address
                     -- 6 bytes long
                     if dest_count < 5 then
-                        sig_data <= "11000011";
+                        var_data := "11000011";
                         dest_count <= dest_count + 1;
                     else
-                        sig_data <= "11000011";
+                        var_data := "11000011";
                         state <= SRC_ADDR;
                     end if;
                 
@@ -75,10 +96,10 @@ begin
                     -- again hardcoded MAC address
                     -- again 6 bytes long (LSB of first byte must be 0)
                     if src_count < 5 then
-                        sig_data <= "01000010";
+                        var_data := "01000010";
                         src_count <= src_count + 1;
                     else
-                        sig_data <= "01000010";
+                        var_data := "01000010";
                         state <= LENGTH;
                     end if;
                 
@@ -86,29 +107,36 @@ begin
                     -- length of payload/data 
                     -- harcoded to 20 bytes
                     if length_count < 1 then
-                        sig_data <= "00010100";
+                        var_data := "00010100";
                         length_count <= length_count + 1;
                     else
-                        sig_data <= "00000000";
+                        var_data := "00000000";
                         state <= DATA;
                     end if;
                 
                 when DATA =>
                     if data_count < DATA_LENGTH - 1 then
-                        sig_data <= "10000001";
+                        var_data := "10000001";
                         data_count <= data_count + 1;
                     else
-                        sig_data <= "10000001";
+                        var_data := "10000001";
                         state <= CRC;
                     end if;
                 
                 when CRC =>
                     state <= IDLE;
-                
+                    sig_crc <= not var_crc;
                 when others =>
                     state <= IDLE;
                     
             end case;
+            
+            if state /= PREAMBLE and state /= SFD and state /= CRC and state /= IDLE then
+              var_crc := crc32_update(var_crc, var_data);  -- byte-wise LFSR function, LSB-first
+            end if;
+            
+            sig_running_crc <= var_crc;
+            sig_data <= var_data;
         end if;
     end process;
     -- this logic is used to prevent one cycle latency from when 
